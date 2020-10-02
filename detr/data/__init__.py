92860a5a29f42b5d3e9c35be67ba0787f063c6ea
Justus Schock <12886177+justusschock@users.noreply.github.com>
2020-10-02 10:09:34 +0200

    Update __init__.py

--- detr/data/__init__.py
+++ detr/data/__init__.py
@@ -6,19 +6,20 @@ from torch.utils.data.dataloader import default_collate as _default_collate
 
 
 class DetrDictCollate:
-    def __init__(self, key: str = 'data'):
+    def __init__(self, key: str = 'data', wrapped_collate: Callable = _default_collate):
         self.key = key
+        self.wrapped_collate = wrapped_collate
         
     def __call__(self, data):
         
         # already padded
         if 'padding_mask' in data[0]:
-            return _default_collate(data)
+            return self._wrapped_collate(data)
         
         collate_separate = [_data.popitem(self.key) for _data in data]
         
         # collate everything else
-        collated = _default_collate(data)
+        collated = self._wrapped_collate(data)
         
         tensor, mask = _pad(collate_separate)
         collated[self.key] = tensor
