b4a29c663edfc7ebde811fba6e9c5e3cbceb1f24
Aditya Oke <okeaditya315@gmail.com>
2020-10-02 13:54:09 +0530

    Fixes pop error

--- detr/data/__init__.py
+++ detr/data/__init__.py
@@ -9,20 +9,20 @@ class DetrDictCollate:
     def __init__(self, key: str = 'data', wrapped_collate: Callable = _default_collate):
         self.key = key
         self.wrapped_collate = wrapped_collate
-        
+
     def __call__(self, data):
-        
+
         # already padded
         if 'padding_mask' in data[0]:
             return self._wrapped_collate(data)
-        
-        collate_separate = [_data.popitem(self.key) for _data in data]
-        
+
+        collate_separate = [_data.pop(self.key) for _data in data]
+
         # collate everything else
         collated = self._wrapped_collate(data)
-        
+
         tensor, mask = _pad(collate_separate)
         collated[self.key] = tensor
         collated['padding_mask'] = mask
-        
+
         return collated
